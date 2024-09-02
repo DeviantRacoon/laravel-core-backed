6916cf6bbe6df880001b2ffb0b3ad2d65cad227b
JHectorZ <hector.zavala@innovacionescomputacionalesvl.com>
2024-09-01 18:17:38 -0700

    [ mejoras ] se ha mejorado las validaciones

--- app/Modules/Core/Http/Validators/PersonValidator/UpdatePersonRequest.php
+++ app/Modules/Core/Http/Validators/PersonValidator/UpdatePersonRequest.php
@@ -15,11 +15,25 @@ class UpdatePersonRequest extends FormRequest
 
     public function rules()
     {
-        $id = $this->input('id');
         return [
-            'id'        => 'required|integer|exists:catalog_persons,id',
-            'birthDate' => 'date_format:Y-m-d',
-            'gender'    => 'string|max:1',
+            'id'                         => 'required|integer|exists:catalog_persons,id',
+            'birthDate'                  => 'date_format:Y-m-d',
+            'gender'                     => 'string|max:1',
+
+            'additionalData.curp'        => 'string|max:18|min:18|unique:catalog_person_additional_data,curp',
+            'additionalData.cellphone'   => 'string|unique:catalog_person_additional_data,cellphone',
+            'additionalData.photo'       => 'string|max:255',
+
+            'addresses.id'               => 'integer|exists:catalog_person_addresses,id',
+            'addresses.street'           => 'string|min:3',
+            'addresses.exteriorNumber'   => 'string|min:3',
+            'addresses.interiorNumber'   => 'string|min:3',
+            'addresses.neighborhood'     => 'string|min:3',
+            'addresses.addressReference' => 'string|min:3',
+            'addresses.municipality'     => 'string|min:3',
+            'addresses.state'            => 'string|min:3',
+            'addresses.country'          => 'string|min:3',
+            'addresses.postalCode'       => 'string|max:5|min:5',
 
         ];
     }
@@ -49,6 +63,20 @@ class UpdatePersonRequest extends FormRequest
             'birthDate.date_format' => 'La fecha de nacimiento debe ser una fecha válida.',
             'gender.string'         => 'El genero debe ser una cadena de texto.',
             'gender.max'            => 'El genero debe tener un máximo de 1 caracteres.',
+
+            'additionalData.curp.unique'      => 'La CURP ya existe.',
+            'additionalData.curp.max'         => 'La CURP debe tener un máximo de 18 caracteres.',
+            'additionalData.curp.min'         => 'La CURP debe tener un mínimo de 18 caracteres.',
+            'additionalData.cellphone.unique' => 'El celular ya existe.',
+            'additionalData.cellphone.max'    => 'El celular debe tener un máximo de 10 caracteres.',
+            'additionalData.cellphone.min'    => 'El celular debe tener un mínimo de 10 caracteres.',
+            'additionalData.photo.max'        => 'La foto de la imagen es demasiado grande.',
+
+            'addresses.id.integer'            => 'El id de la dirección debe ser un valor numérico.',
+            'addresses.id.exists'             => 'El id de la dirección no existe.',
+            'addresses.street.string'         => 'La calle debe ser una cadena de texto.',
+            'addresses.street.max'            => 'La calle debe tener un máximo de 100 caracteres.',
+            'addresses.street.min'            => 'La calle debe tener un mínimo de 3 caracteres.',
         ];
     }
 }
